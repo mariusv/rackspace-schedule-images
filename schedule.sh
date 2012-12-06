4ab3cc8452c3f0d9d2b74cc1c0d31825c0cd059d
Marius Voila <marius.voila@gmail.com>
2012-12-06 11:55:34 +0000

    adding confirmation

--- schedule.sh
+++ schedule.sh
@@ -5,8 +5,10 @@ else
         echo "Impossible to run this script because I can't find the config file!!!"
 fi
 HOSTNAME='server'
-TODAY=$(date +"%m_%d_%Y")
-FIVE_DAYS_AGO=$(date --date='5 days ago' +"%m_%d_%Y")
-source /root/.nova
+TODAY=$(date +"%H_%M_%m_%d_%Y")
+FIVE_DAYS_AGO=$(date --date='5 days ago' +"%H_%M_%m_%d_%Y")
 nova image-create $HOSTNAME $HOSTNAME-$TODAY
+if [[ -z 'nova image-list | grep $HOSTNAME-$TODAY SAVING']]; then
+echo "Image was successful"
+
 nova image-delete $HOSTNAME-$FIVE_DAYS_AGO
