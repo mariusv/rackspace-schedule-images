0e66ad8271a368be3379b6edda57252936104de7
Marius Voila <marius.voila@gmail.com>
2012-12-08 16:39:18 +0000

    moved everything in the config file

--- schedule.sh
+++ schedule.sh
@@ -4,19 +4,20 @@ source '/root/nova'
 else
 echo "Impossible to run this script because I can't find the config file!"
 fi
-HOSTNAME='the_server_you_want_to_take_image_of'
-TODAY=$(date +"%M_%H_%d_%m_%Y")
-FIVE_DAYS_AGO=$(date --date='5 days ago' +"%M_%H_%d_%m_%Y")
-nova image-create $HOSTNAME $HOSTNAME-$TODAY
-sleep 2h
-echo "nova image-list | grep $HOSTNAME-$TODAY | grep SAVING"
-if [[ -f 'nova image-list | grep $HOSTNAME-$TODAY | grep SAVING' ]]; then
-echo ${BODYERR}| mail -s ${SUBJECT} ${TO_ADDRESS}
+#nova image-create $HOSTNAME $HOSTNAME-$TODAY
+#sleep 2h
+#echo "nova image-list | grep $HOSTNAME-$TODAY | grep SAVING"
+#if [[ -f 'nova image-list | grep $HOSTNAME-$TODAY | grep SAVING' ]]; then
+#echo ${BODYERR}| mail -s ${SUBJECT} ${TO_ADDRESS}
 
+#else
+
+#echo ${BODY}| mail -s ${SUBJECT} ${TO_ADDRESS}
+
+#fi
+
+if [[ "$DELETE" = 'yes' ]]; then
+   echo "nova image-delete $HOSTNAME-$FIVE_DAYS_AGO"
 else
-
-echo ${BODY}| mail -s ${SUBJECT} ${TO_ADDRESS}
-
+exit 1
 fi
-
-nova image-delete $HOSTNAME-$FIVE_DAYS_AGO
